6495eac22880a846f08359b6c0686205341fecef
dburnaye <dburnaye@gmu.edu>
2025-05-23 05:25:14 -0400

    Implemented user-friendly menu

--- finance_tracker.py
+++ finance_tracker.py
@@ -7,6 +7,9 @@
 class EmptyValueError(ValueError):
     pass
 
+class IncorrectInputValueError(ValueError):
+    pass
+
 def check_for_empty(given_str):
     return len(given_str) == 0
 
@@ -34,9 +37,9 @@ def expense_prompt():
                 amount = float(amount)
             
         except EmptyValueError:
-            print(f"Make sure that when entering input in general, it is not empty/nothing. Give it another shot.\n")
+            print(f"Make sure that when entering input in general, it is not empty/nothing. Give this another shot.\n")
         except ValueError:
-            print(f"Make sure that when entering input for the amount, it is a decimal number, not text or anything else. Give it another try.\n")
+            print(f"Make sure that when entering input for the amount, it is a decimal number, not text or anything else. Give this another run.\n")
         else:
             print("Expense added successfully.\n")
             return descr, category, amount
@@ -55,16 +58,31 @@ def exec_finance_tracker():
     expenses = []
     categories = {}
     while True:
-        expense_info = expense_prompt()
-        descr = expense_info[0]
-        category = expense_info[1]
-        amount = expense_info[2]
+            try:
+                print("What would you like to do?\n1. Add Expense\n2. View All Expenses\n3. View Summary\n4. Exit")
+                select_num = int(input("Choose an option: "))
+                
+                if select_num == 1:
+                    expense_info = expense_prompt()
+                    descr = expense_info[0]
+                    category = expense_info[1]
+                    amount = expense_info[2]
 
-        expenses.append({category: (descr, amount)})
-        categories.update({category: categories.get(category) + amount} if (category in categories) else {category: amount})
-
-        view_expenses(expenses)
-        view_summary(categories)
+                    expenses.append({category: (descr, amount)})
+                    categories.update({category: categories.get(category) + amount} if (category in categories) else {category: amount})
+                elif select_num == 2:
+                    view_expenses(expenses)
+                elif select_num == 3:
+                    view_summary(categories)
+                elif select_num == 4:
+                    print("Goodbye!\n")
+                    break
+                else:
+                    raise IncorrectInputValueError()
+            except IncorrectInputValueError:
+                print("Make sure that when selecting from the number options, it is a number from 1-4, nothing else. Give this another try.\n")
+            except ValueError:
+                print("Make sure that instead of a string or some other value, have it be a number from 1-4. Give this another try.\n")
 
 def start_program():
     print("Welcome to the personal finance tracker!\n")
